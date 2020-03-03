2e38b472217ff02a7c78009a6796ab114386a2b2
xivi0n <mihailo.jovanovich@gmail.com>
2020-03-03 17:19:34 +0100

    calc 1.0

---
+++ js/script.js
@@ -0,0 +1,70 @@
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function budzet(){
+    var budzet = parseFloat(document.getElementById("budzet").value);
+    var kvota1 = parseFloat(document.getElementById("kvota1").value);
+    var kvota2 = parseFloat(document.getElementById("kvota2").value);
+    var ulog1 = document.getElementById("ulog1");
+    var ulog2 = document.getElementById("ulog2");
+    var profit = document.getElementById("profit");
+    var profit_ukupno = document.getElementById("profit_ukupno");
+    if (kvota1>kvota2){
+        var pom = kvota1;
+        kvota1 = kvota2;
+        kvota2 = pom;
+    }
+    var diff = kvota1/kvota2;
+    var f_in1 = budzet/(1+diff);
+    var f_in2 = diff*f_in1;
+    var pr = kvota1*f_in1-(f_in1+f_in2);
+
+    ulog1.innerHTML = numberWithCommas(f_in1.toFixed(2));
+    ulog2.innerHTML = numberWithCommas(f_in2.toFixed(2));
+    profit.innerHTML = (100*pr/budzet).toFixed(2)+"%";
+    profit_ukupno.innerHTML = numberWithCommas(pr.toFixed(2));
+}
+
+function ulog(){
+    var f_in1 = parseFloat(document.getElementById("ulog").value);
+    var kvota1 = parseFloat(document.getElementById("kvota1_ulog").value);
+    var kvota2 = parseFloat(document.getElementById("kvota2_ulog").value);
+    var ulog1 = document.getElementById("ulog1");
+    var ulog2 = document.getElementById("ulog2");
+    var profit = document.getElementById("profit");
+    var profit_ukupno = document.getElementById("profit_ukupno");
+    var f_in2 = f_in1*kvota1/kvota2;
+    var pr = kvota1*f_in1-(f_in1+f_in2);
+    var budzet = f_in1 + f_in2;
+    ulog1.innerHTML = numberWithCommas(f_in1.toFixed(2));
+    ulog2.innerHTML = numberWithCommas(f_in2.toFixed(2));
+    profit.innerHTML = (100*pr/budzet).toFixed(2)+"%";
+    profit_ukupno.innerHTML = numberWithCommas(pr.toFixed(2));
+}
+
+function change_tab(tab){
+    var main1 = document.getElementById("main1")
+    var main2 = document.getElementById("main2")
+    var tab1 = document.getElementById("tab1")
+    var tab2 = document.getElementById("tab2")
+    if(tab == 1 && current_tab == 2){
+        current_tab = 1
+        main2.classList.add("hidden")
+        main1.classList.remove("hidden")
+        tab1.classList.add("selected")
+        tab2.classList.remove("selected")
+        budzet()
+    }
+    else if(tab == 2 && current_tab == 1){
+        current_tab = 2
+        tab2.classList.add("selected")
+        tab1.classList.remove("selected")
+        main1.classList.add("hidden")
+        main2.classList.remove("hidden")
+        ulog()
+    }
+}
+current_tab = 1
+
+
